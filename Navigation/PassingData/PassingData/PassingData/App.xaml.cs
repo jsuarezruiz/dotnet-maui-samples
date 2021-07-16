0e1698cab9c77a43ee145df64804b512772e73f5
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-07-16 20:21:23 +0200

    PassingData sample

---
+++ Navigation/PassingData/PassingData/PassingData/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using Application = Microsoft.Maui.Controls.Application;
+
+namespace PassingData
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage(DateTime.Now.ToString("u")));
+        }
+    }
+}
---
+++ Navigation/PassingData/PassingData/PassingData/SecondPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace PassingData
+{
+    public partial class SecondPage : ContentPage
+	{
+		public SecondPage ()
+		{
+			InitializeComponent ();
+		}
+
+		async void OnNavigateButtonClicked (object sender, EventArgs e)
+		{
+			await Navigation.PopAsync ();
+		}
+	}
+}
+
