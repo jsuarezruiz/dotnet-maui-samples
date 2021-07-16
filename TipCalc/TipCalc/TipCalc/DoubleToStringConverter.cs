3b060674976054987de9bdffe9dcd614200d896c
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-07-16 19:52:41 +0200

    TipCalc sample

---
+++ TipCalc/TipCalc/TipCalc/DoubleToStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Globalization;
+
+namespace TipCalc
+{
+    public class DoubleToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, 
+                              object parameter, CultureInfo culture)
+        {
+            // Assumes value is double.
+            double number = (double)value;
+
+            // Return empty string for a zero (good for Entry views).
+            if (number == 0)
+            {
+                return "";
+            }
+
+            return number.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, 
+                                  object parameter, CultureInfo culture)
+        {
+            double number = 0;
+            Double.TryParse((string)value, out number);
+            return number;
+        }
+    }
+}
---
+++ TipCalc/TipCalc/TipCalc/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Controls.Xaml;
+
+[assembly: XamlCompilationAttribute(XamlCompilationOptions.Compile)]
+
+namespace TipCalc
+{
+    public class Startup : IStartup
+    {
+        public void Configure(IAppHostBuilder appBuilder)
+        {
+            appBuilder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+        }
+    }
+}
