53cd72732d7223ea7f90eda2fad05fb2bf77bb9d
Javier Suárez <javiersuarezruiz@hotmail.com>
2021-08-08 18:40:21 +0200

    BugSweeper sample

---
+++ BugSweeper/BugSweeper/BugSweeper/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Controls.Xaml;
+using Microsoft.Maui.Hosting;
+
+[assembly: XamlCompilationAttribute(XamlCompilationOptions.Compile)]
+
+namespace BugSweeper
+{
+    public class Startup : IStartup
+    {
+        public void Configure(IAppHostBuilder appBuilder)
+        {
+            appBuilder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+        }
+    }
+}
